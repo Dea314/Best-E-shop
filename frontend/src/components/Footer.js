f3483cb85eb0ab3069e18763c31382de41c0396a
Dea314 <fabrisdanijela@gmail.com>
2022-11-16 13:13:33 +0100

    style

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
+import dflogo from "../dflogo.png";
 
 const Footer = () => {
   return (
@@ -6,7 +7,17 @@ const Footer = () => {
       <Container fluid>
         <Row className="mt-5">
           <Col className="bg-secondary text-center py-4">
-            Copyright &copy; {new Date().getFullYear()} - All Rights Reserved
+            Copyright &copy; {new Date().getFullYear()} - All Rights Reserved -
+            Made with ❤️ by Dea314{"  "}
+            <span>
+              <img
+                className="logo"
+                src={dflogo}
+                alt="Dea314"
+                width="45"
+                height="45"
+              />
+            </span>
           </Col>
         </Row>
       </Container>
